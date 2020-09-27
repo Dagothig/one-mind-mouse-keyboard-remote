60e22f8d33881c501ce378619be95e98a1ae6d43
Dagothig <guillaumenoelmartel@gmail.com>
2020-09-27 12:38:30 -0400

    Server

--- remote-hotswap.py
+++ remote-hotswap.py
@@ -4,44 +4,43 @@
 import sys
 import keyboard
 import mouse
-from socketserver import BaseRequestHandler, UDPServer
+from pythonosc.dispatcher import Dispatcher
+from pythonosc.osc_server import BlockingOSCUDPServer
 import struct
 from enum import Enum
 from threading import Thread
 
 # FIXME: keyboard doesn't understand scan codes unless we use keys at some point...
 keyboard.key_to_scan_codes('b')
+keyboard.on_press(lambda x: print(x.scan_code))
 
-class KeyboardUDPHandler(BaseRequestHandler):
-    def handle(self):
-        data = self.request[0]
-        for t, c, v in struct.iter_unpack("x" * 16 + "HHI", data):
-            if c == 0:
-                continue
-            if v == 1:
-                keyboard.press(c)
-            elif v == 0:
-                keyboard.release(c)
-            elif v == 2:
-                keyboard.send(c)
+def on_key_press(address, key):
+    keyboard.press(key)
 
-class MouseUDPHandler(BaseRequestHandler):
-    def handle(self):
-        data = self.request[0]
-        for v, x, y in struct.iter_unpack("bbb", data):
-            left = v & 1
-            if left and not mouse.is_pressed('left'):
-                mouse.press('left')
-            if not left and mouse.is_pressed('left'):
-                mouse.release('left')
-            
-            right = (v >> 1) & 1
-            if right and not mouse.is_pressed('right'):
-                mouse.press('right')
-            if not right and mouse.is_pressed('right'):
-                mouse.release('right')
+def on_key_release(address, key):
+    keyboard.release(key)
 
-            mouse.move(x, -y, False)
+def on_move(address, x, y):
+    mouse.move(x, y)
 
-with UDPServer(('', 8000), KeyboardUDPHandler) as server:
+def on_button(address, ev):
+    event_type, button = ev
+    if event_type ==  mouse.UP:
+        mouse.release(button)
+    elif event.type ==  mouse.DOWN:
+        mouse.press(button)
+    elif event.type ==  mouse.DOUBLE:
+        mouse.double_click(button)
+
+def on_wheel(address, delta):
+    mouse.wheel(delta)
+
+dispatcher = Dispatcher()
+dispatcher.map("/mouse/move", on_move)
+dispatcher.map("/mouse/button", on_button)
+dispatcher.map("/mouse/wheel", on_wheel)
+dispatcher.map("/keyboard/press", on_key_press)
+dispatcher.map("/keyboard/release", on_key_release)
+
+with BlockingOSCUDPServer(('', 8000), dispatcher) as server:
     server.serve_forever()
